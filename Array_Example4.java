3848e0346efba2fcc5271eb8474a7a2b4e05c6ae
Ambika Sahu <ambikasahu268@gmail.com>
2023-01-09 10:50:55 +0530

    first commit

---
+++ Array_Example4.java
@@ -0,0 +1,12 @@
+class Array_Example4
+{
+   public static void main(String[] args) 
+   {
+      int arr[][]= {{1,2},{3,4}};
+      System.out.println(arr[0][0]);
+      System.out.println(arr[0][1]);
+      System.out.println(arr[1][0]);
+      System.out.println(arr[1][1]);
+
+   } 
+}
